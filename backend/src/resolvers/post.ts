d1eea12392cf7cf8a5eb23204f2aeeb42b07ba28
Ahmed Belal <ahmbelal95@gmail.com>
2021-10-10 16:33:57 +0200

    basic crud operations

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
@@ -15,5 +15,50 @@ export class PostResolver {
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { _id: id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => String) id: number,
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { _id: id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => String) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    try {
+      const res = await em.nativeDelete(Post, { _id: id });
+      if (res === 0) {
+        throw new Error("No post found with the id of " + id);
+      }
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
   }
 }
